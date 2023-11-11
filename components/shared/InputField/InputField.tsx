c62caa202124b1b68e65a7ec33f68ebbc50983d5
Harshit Yadav <harshit yadav>
2023-11-11 22:20:51 +0530

    made minor corrections

--- components/shared/InputField/InputField.tsx
+++ components/shared/InputField/InputField.tsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import React, { ChangeEventHandler } from 'react'
+interface InputFieldProps {
+  type: string;
+  placeholder: string;
+  htmlFor: string;
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+  value: number | string;
+  labelText: string;
+}
 
-const InputField = ({ type, placeholder, htmlFor, onChangeHandler, value, labelText }) => {
-    return (
 
-        <>
-        <div className="mt-4 w-full">
-         <label
-                htmlFor={htmlFor}
-                className="font-inter text-sm font-normal leading-6"
-              >
-                {labelText}
-              </label>
-            <input className="mt-2 bg-white-300 px-4 py-[17px] w-full rounded-sm" type={type} placeholder={placeholder} value={value}
-    onChange = { onChangeHandler }
-/>    
-</div>
+const InputField = ({ type, placeholder, htmlFor, onChangeHandler, value, labelText }: InputFieldProps) => {
+  return (
+
+    <>
+      <div className="mt-4 w-full">
+        <label
+          htmlFor={htmlFor}
+          className="font-inter text-sm font-normal leading-6"
+        >
+          {labelText}
+        </label>
+        <input className="mt-2 bg-white-300 px-4 py-[17px] w-full rounded-sm" type={type} placeholder={placeholder} value={value}
+          onChange={onChangeHandler}
+        />
+      </div>
     </>
-    )
+  )
 
 }
 
 export default InputField
-/*
-   <InputField type={"number"} htmlFor={"sale-price"} placeholder={"Sales price"} value={salesPrice} onChangeHandler={(e: React.ChangeEvent<HTMLInputElement>) =>
-                  setSalesPrice(parseInt(e.target.value)} labelText={"Enter the sale price of Crypto"} />
-*/
